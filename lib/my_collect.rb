c3073171a99bad328961bb468050cad4014a399b
Owen Mowery <owenmowery99@gmail.com>
2021-02-24 19:18:33 -0500

    commit tree 1de89c2c4411061b1f70cf3d788aa282c67b0baa

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(array)
+    i = 0
+    new_array = []
 
+    while i < array.size
+        block_given?
+        new_array << yield(array[i])
+        i += 1
+    end
+    new_array
+end
 
